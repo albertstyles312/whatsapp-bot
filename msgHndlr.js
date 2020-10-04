ef963dc6f2ad6c987cff48cd9adf1a7ebfda241a
Agus Purba <57493908+agusydk3@users.noreply.github.com>
2020-10-04 20:35:22 +0700

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -112,7 +112,7 @@ module.exports = msgHandler = async (client, message) => {
             break
         case '!donasi':
         case '!donate':
-            client.sendLinkWithAutoPreview(from, 'https://saweria.co/donate/agusydk3', donate)
+            client.sendImage(from, './media/img/qris.jpeg', donate)
             break
         case '!tts':
             if (args.length === 1) return client.reply(from, 'Kirim perintah *!tts [id, en, jp, ar] [teks]*, contoh *!tts id halo semua*')
